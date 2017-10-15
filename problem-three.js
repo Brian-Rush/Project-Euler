5bf8fbba32c69879bb48dddbac61662d7abff76e
Brian-Rush <yankeh@gmail.com>
2017-10-15 01:56:50 -0700

    fourth attempt, successful but still very inefficient, took around an hour for comp to generate the answer

--- problem-three.js
+++ problem-three.js
@@ -4,68 +4,28 @@
 //
 // What is the largest prime factor of the number 600851475143 ?
 
-///////////////////////////////////
-// Saturday 5pm
-///////////////////////////////////
+/////////////////////////
+//Saturday 11:24pm
+//////////////////////////
 
-//Function for determining if a number is prime:
-
-var isPrime = input => {
-  let prime = true;
-  for (i = Math.floor(Math.sqrt(input)); i >= 2; i--) {
-    if (input % i === 0) {
-      prime = false;
-      break;
-    }
-  }
-  return prime;
-}
-
-isPrime(14);
-
-//Function for determining if a number is a factor of the given number:
-
-var isFactor = (dividend, divisor) => {
-  let factor = false;
-  if (dividend % divisor === 0) {
-    factor = true;
-  }
-  return factor;
-}
-
-var isBoth = (bigNum) => {
-  for (littleNum = Math.floor(bigNum / 2); littleNum > 1; littleNum--) {
-    isFactor(bigNum, littleNum);
-    console.log(isFactor);
-    if (isFactor) {
-      isPrime(littleNum);
-      if (isPrime = true) {
-        console.log(littleNum);
+// var givenNum = 28;
+var givenNum = 600851475143
+for (let i = Math.floor(givenNum / 2); i > 1; i--) {
+  if (givenNum % i == 0) {
+    // console.log("factor:" + i);
+    var prime = true;
+    for (let j = Math.floor(Math.sqrt(i)); j > 1; j--) {
+      if (i % j == 0) {
+        prime = false;
+        // console.log("composite:" + i + "(" + j + ")");
         break;
       }
     }
+    if (prime) {
+      console.log(i, prime);
+      break;
+  }
   }
 }
 
-isBoth(28);
-
-
-
-
-var givenNum = 28;
-for (i = Math.floor(givenNum / 2); i > 0; i--) {
-  if (givenNum % i === 0) {
-    for (thisNum = Math.floor(Math.sqrt(i)); thisNum > 1; thisNum--) {
-      if (i % thisNum === 0) {
-        prime = false;
-      } else {
-        prime = true;
-        console.log(i, thisNum);
-      }
-    }
-  }
-}
-if (prime = true) {
-  console.log(i, thisNum);
-  break;
-}
+///trying switch///
